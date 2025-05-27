103feafdb2293fcad6353af3db305d5f1b637996
Tianon Gravi <admwiggin@gmail.com>
2025-05-27 15:05:42 -0700

    Add initial `generate-stackbrew-library.sh`

---
+++ generate-stackbrew-library.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+commit="$(git log -1 --format='format:%H' HEAD --)"
+
+from="$(git show "$commit:Dockerfile" | awk '$1 == "FROM" && $2 == "--platform=$TARGETPLATFORM" { from = $3 } END { print from }')" # sweep sweep sweep
+
+arches="$(bashbrew remote arches --json "$from")"
+arches="$(jq <<<"$arches" --raw-output '.arches | keys | join(", ")')"
+
+cat <<-EOF
+# this file is generated via https://github.com/tianon/containerd-registry/blob/$commit/generate-stackbrew-library.sh
+
+Maintainers: Tianon Gravi <admwiggin@gmail.com> (@tianon)
+GitRepo: https://github.com/tianon/containerd-registry.git
+GitFetch: refs/heads/main
+GitCommit: $commit
+
+Tags: latest
+EOF
